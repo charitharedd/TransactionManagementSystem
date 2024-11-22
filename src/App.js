9a3783acd6c231cfb77f03e79e271485f5221f3d
charitharedd <129164869+charitharedd@users.noreply.github.com>
2024-11-22 12:22:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,28 @@
 
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import CreateTransaction from './components/CreateTransaction';
+import TransactionList from './components/TransactionList';
+import UpdateTransaction from './components/UpdateTransaction';
+import TransactionDetails from './components/TransactionDetails';
+
+const App = () => {
+  return (
+    <div>
+      <nav>
+        <Link to="/">Home</Link> | 
+        <Link to="/create">Create Transaction</Link> | 
+        <Link to="/transactions">Transaction History</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<h1>Welcome to Transaction Management</h1>} />
+        <Route path="/create" element={<CreateTransaction />} />
+        <Route path="/transactions" element={<TransactionList />} />
+        <Route path="/transactions/:id" element={<TransactionDetails />} />
+        <Route path="/transactions/update/:id" element={<UpdateTransaction />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
